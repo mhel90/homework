f2fe695513ff33f7fabfe9109f0db9bb69c9c5e4
mhelm90 <magdalena.h.morawiec@gmail.com>
2023-07-17 15:47:09 +0200

    init

---
+++ cypress/pages/detailsPage.js
@@ -0,0 +1,33 @@
+export class DetailsPage {
+    get name() {
+        return cy.get('.ng-binding').eq(0)
+    }
+
+    get description() {
+        return cy.get('.ng-binding').eq(2)
+    }
+
+    get when() {
+        return cy.get('.ng-binding').eq(1)
+    }
+
+    get favoriteHint() {
+        return cy.get('i')
+    }
+
+    get deleteFromFavoriteIcon() {
+        return cy.get('[alt="add to fav"]')
+    }
+
+    get addToFavoriteIcon() {
+        return cy.get('[alt="delete fav"]')
+    }
+
+    get backButton() {
+        return cy.get('.navBtn')
+    }
+
+    get coverImage() {
+        return cy.get('alt=["show cover image"]')
+    }
+}
